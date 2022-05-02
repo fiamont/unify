d528826e9569c2efaa241599d73f7caa2a74ebca
Nicole <nicole.silfverling@gmail.com>
2022-05-02 17:23:08 +0200

    clean up styling functions

--- components/Event.jsx
+++ components/Event.jsx
@@ -5,104 +5,66 @@ import style from '../styles/Event.module.css'
 
 function Event(){
  
-    function outerBoarder (category) {
+    function categoryColors (category) {
 
-        let categoryClassName = "";
-
-        if(category == "Uteliv" || category == "Konsert" || category == "Quiz") {
-            categoryClassName = style.eventboxUtelivKonsertQuiz;
-        }
-        else if(category == "Kultur & Livsstil"){
-            categoryClassName = style.eventboxKulturLivstil;
-        }
-        else if(category == "Sport & Fritid" || category == "Guider"){
-            categoryClassName = style.eventboxSportFritidGuider;
-        }
-        else if(category == "Mat & Dryck"){
-            categoryClassName = style.eventboxMatDryck;
-        }
-        else if(category == "Konst & Hantverk"){
-            categoryClassName = style.eventboxKonstHantverk;
-        }else if(category == "Hälsa & Skönhet"){
-            categoryClassName = style.eventboxHalsaSkonhet;
-        }
-        else
-        categoryClassName = style.eventboxDefault;
-
-        return categoryClassName;
-
-    }
-
-    function innerBoarder (category) {
-
-        let categoryClassName = "";
+        let outerBorder = "";
+        let innerBorder = "";
+        let categoryBox = "";
 
         if(category == "Uteliv" || category == "Konsert" || category == "Quiz") {
-            categoryClassName = style.innerBoarderUtelivKonsertQuiz;
+            outerBorder= style.eventboxUtelivKonsertQuiz;
+            innerBorder = style.innerBorderUtelivKonsertQuiz;
+            categoryBox = style.categoryUtelivKonsertQuiz;
         }
         else if(category == "Kultur & Livsstil"){
-            categoryClassName = style.innerBoarderKulturLivstil;
+            outerBorder = style.eventboxKulturLivstil;
+            innerBorder = style.innerBorderKulturLivstil;
+            categoryBox = style.categoryKulturLivstil;
         }
         else if(category == "Sport & Fritid" || category == "Guider"){
-            categoryClassName = style.innerBoarderSportFritidGuider;
+            outerBorder = style.eventboxSportFritidGuider;
+            innerBorder = style.innerBorderSportFritidGuider;
+            categoryBox = style.categorySportFritidGuider;
         }
         else if(category == "Mat & Dryck"){
-            categoryClassName = style.innerBoarderMatDryck;
+            outerBorder = style.eventboxMatDryck;
+            innerBorder = style.innerBorderMatDryck;
+            categoryBox = style.categoryMatDryck;
         }
         else if(category == "Konst & Hantverk"){
-            categoryClassName = style.innerBoarderKonstHantverk;
-        }else if(category == "Hälsa & Skönhet"){
-            categoryClassName = style.innerBoarderHalsaSkonhet;
+            outerBorder = style.eventboxKonstHantverk;
+            innerBorder = style.innerBorderKonstHantverk;
+            categoryBox = style.categoryKonstHantverk;
         }
-        else
-        categoryClassName = style.innerBoarderDefault;
-
-        return categoryClassName;
+        else if(category == "Hälsa & Skönhet"){
+            outerBorder = style.eventboxHalsaSkonhet;
+            innerBorder = style.innerBorderHalsaSkonhet;
+            categoryBox = style.categoryHalsaSkonhet;
+        }
+        else{
+            outerBorder = style.eventboxDefault;
+            innerBorder = style.innerBorderDefault;
+            categoryBox = style.categoryDefault;
+        }
+              
+        return [outerBorder, innerBorder, categoryBox];
 
     }
-
-    function category (category) {
-
-        let categoryClassName = "";
-
-        if(category == "Uteliv" || category == "Konsert" || category == "Quiz") {
-            categoryClassName = style.categoryUtelivKonsertQuiz;
-        }
-        else if(category == "Kultur & Livsstil"){
-            categoryClassName = style.categoryKulturLivstil;
-        }
-        else if(category == "Sport & Fritid" || category == "Guider"){
-            categoryClassName = style.categorySportFritidGuider;
-        }
-        else if(category == "Mat & Dryck"){
-            categoryClassName = style.categoryMatDryck;
-        }
-        else if(category == "Konst & Hantverk"){
-            categoryClassName = style.categoryKonstHantverk;
-        }else if(category == "Hälsa & Skönhet"){
-            categoryClassName = style.categoryHalsaSkonhet;
-        }
-        else
-        categoryClassName = style.categoryDefault;
-
-        return categoryClassName;
-
-    }
-
     return(
         
         <div className={style.container}>
             {listOfEvents.map((listOfEvents) =>(
-                <div key={listOfEvents.title} className={outerBoarder(listOfEvents.category)}>
-                    <div className={innerBoarder(listOfEvents.category)}>
+                <div key={listOfEvents.title} className={categoryColors(listOfEvents.category)[0]}>
+                    <div className={categoryColors(listOfEvents.category)[1]}>
                         <div className={style.eventimg}>
-                            <div className={category(listOfEvents.category)}>
+                            <div className={categoryColors(listOfEvents.category)[2]}>
                                 <p className={style.categoryText}>{listOfEvents.category}</p>
                             </div>
                             <div className={style.event}>
                                 <p className={style.title}>{listOfEvents.title}</p>
                                 <p className={style.description}>{listOfEvents.body}</p>
-                                
+                                <p></p>
+                               
                             
                             </div>
                         </div>
