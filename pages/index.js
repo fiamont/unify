5dd4d66821fb6c9b2971990c4883b3eb8cb31c6c
Juliosc1 <julio.siklander@gmail.com>
2022-05-12 11:23:11 +0200

    Firebase Setup v2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Eventbutton from '../components/Eventbutton'
 import Event from '../components/Event' 
