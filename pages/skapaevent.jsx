9a8a3d8c12de0054726a1aa9cb34e866c7ba75bb
Sofia <sofia.montgomery@gmail.com>
2022-05-17 16:44:25 +0200

    adjusted css and commented out pictures

--- pages/skapaevent.jsx
+++ pages/skapaevent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head'
 import NewPost from '../components/new';
+import styles from '../styles/Home.module.css'
 
 
 
@@ -8,7 +9,7 @@ function EventForm(){
     return(
         <div>
             <Head><title>Unify - Skapa event</title></Head>
-            <main>
+            <main className={styles.main}>
               
                 {/* <MultiStepForm /> */}
                 <NewPost />
