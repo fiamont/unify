abf7f881585946c5dd6b8837e8f3fdb63e57cdbf
Nicole <nicole.silfverling@gmail.com>
2022-05-18 10:32:24 +0200

    category images

--- components/Event.jsx
+++ components/Event.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import style from '../styles/Event.module.css'
 import ShowUpBtn from './ShowUp';
 
+
 const Event = ({events}) =>{
  
     // const events = props.events;
@@ -13,36 +14,42 @@ const Event = ({events}) =>{
         let innerBorder = "";
         let categoryBox = "";
         let showUpBtn = '';
+        let eventImage = ""
 
         if(category == "Konsert, Quiz & Uteliv") {
             outerBorder= style.eventboxUtelivKonsertQuiz;
             innerBorder = style.innerBorderUtelivKonsertQuiz;
             categoryBox = style.categoryUtelivKonsertQuiz;
             showUpBtn = '#FFC130';
+            eventImage = <img className={style.categoryImage} src="/konsertquizuteliv.svg"/>
         }
         else if(category == "Kultur & Livsstil"){
             outerBorder = style.eventboxKulturLivstil;
             innerBorder = style.innerBorderKulturLivstil;
             categoryBox = style.categoryKulturLivstil;
             showUpBtn = '#416AD2';
+            eventImage = <img className={style.categoryImage} src="/kulturlivstil.svg"/>
         }
         else if(category == "Sport & Fritid"){
             outerBorder = style.eventboxSportFritid;
             innerBorder = style.innerBorderSportFritid;
             categoryBox = style.categorySportFritid;
             showUpBtn = '#D24341';
+            eventImage = <img className={style.categoryImageSport} src="/sportfritid.svg"/>
         }
         else if(category == "Mat & Dryck"){
             outerBorder = style.eventboxMatDryck;
             innerBorder = style.innerBorderMatDryck;
             categoryBox = style.categoryMatDryck;
             showUpBtn = '#B36D8F';
+            eventImage = <img className={style.categoryImage} src="/matdryck.svg"/>
         }
         else if(category == "Konst & Hantverk"){
             outerBorder = style.eventboxKonstHantverk;
             innerBorder = style.innerBorderKonstHantverk;
             categoryBox = style.categoryKonstHantverk;
             showUpBtn = '#FF9211';
+            eventImage = <img className={style.categoryImage} src="/konsthantverk.svg"/>
         }
         else{
             outerBorder = style.eventboxDefault;
@@ -51,7 +58,7 @@ const Event = ({events}) =>{
             showUpBtn = '#0b4341';
         }
 
-        return {outerBorder, innerBorder, categoryBox, showUpBtn};
+        return {outerBorder, innerBorder, categoryBox, showUpBtn, eventImage};
         
     }
     return(
@@ -61,6 +68,9 @@ const Event = ({events}) =>{
                 <div key={events.id} className={categoryColors(events.category).outerBorder}>{/* outerBorder */}
                     <div className={categoryColors(events.category).innerBorder}> {/* innerBorder */}
                         <div className={style.eventimg}> {/* place for img (currently just gray box) */}
+                            <div className={style.imgWrapper}>
+                                {categoryColors(events.category).eventImage}
+                            </div>
                             <div className={categoryColors(events.category).categoryBox}> {/* the small categoryBox */}
                                 <p className={style.categoryText}>{events.category}</p>
                             </div>
