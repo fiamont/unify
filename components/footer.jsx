46ada158e2d46d23d1abf8366e20266ffd3fbcd2
Sofia <sofia.montgomery@gmail.com>
2022-05-20 15:00:00 +0200

    edit desktopversion

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import style from "./../styles/Footer.module.css"
+import Link from 'next/link'
+
 
 function Footer() {
     return (
-        <div className={style.mainfooter}><p>Copyright &copy; 2022 Awesome Java21 & UX Design</p></div>
+        <div className={style.mainfooter}>
+            <div className={style.text}>
+            <Link href='/kontaktaoss' passHref >
+              <div className={style.item}>Kontakta oss</div>
+            </Link>
+            <Link href='/omoss' passHref >
+              <div className={style.item}>Om oss</div>
+            </Link>
+            </div>
+            <p>Copyright &copy; 2022 Awesome Java21 & UX Design</p>
+        </div>
     )
 }
 
