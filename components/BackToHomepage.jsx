b3836473ae63ba64c3f015a6cd0841b735106102
Sofia <sofia.montgomery@gmail.com>
2022-05-20 23:21:14 +0200

    updated backarrow

--- components/BackToHomepage.jsx
+++ components/BackToHomepage.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
-import Arrow from './Icons/arrow'
 import Image from 'next/image'
 
 function BackToHomepage() {
   return (
     <div>
       <Link href='/' passHref >
-        <Image src="/arrowLeft.png" alt='arrowLeft' width={28} height={28}/>
+        <Image src="/ArrowLeft.png" alt='ArrowLeft' width={28} height={28}/>
       </Link>
     </div>
   )
