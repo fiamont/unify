45434570376512a1c23bf886ea8bb55ca90b19d3
Nicole <nicole.silfverling@gmail.com>
2022-05-23 00:26:53 +0200

    fixed navbar mobile

--- components/Topnavbar.jsx
+++ components/Topnavbar.jsx
@@ -1,17 +1,16 @@
-import style from './../styles/Topnavbar.module.css'
-import Navbar from "./Navbar"
-import SvgUnifyLogo from './Icons/unifyLogo'
+import style from "./../styles/Topnavbar.module.css";
+import Navbar from "./Navbar";
+import SvgUnifyLogo from "./Icons/unifyLogo";
 
-function Topnavbar () {
-    return(
-        <header className={style.topnavbar}>
-            <div className={style.images}>
-            <SvgUnifyLogo className={style.unify}/>
-            </div>
-            <Navbar className={style.navbar}/>
-            {/* <hr className={style.hr}></hr> */}
-
-        </header>
-    )
+function Topnavbar() {
+  return (
+    <header className={style.topnavbar}>
+      <div className={style.images}>
+        <SvgUnifyLogo className={style.unify} />
+      </div>
+      <Navbar className={style.navbar} />
+      {/* <hr className={style.hr}></hr> */}
+    </header>
+  );
 }
-export default Topnavbar
+export default Topnavbar;
