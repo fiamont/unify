2b670f1ad004840dedcde76295a3916cde10f17b
Sofia <sofia.montgomery@gmail.com>
2022-05-23 15:14:03 +0200

    whole category-square klickable

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -15,36 +15,36 @@ function Categories() {
     <div className={style.categoriesbox}>
         <p className={style.title}>KATEGORIER</p>
         <div className={style.gridBox}>
-            <div className={style.category} style={{background: CategorySelector("Nöje & Uteliv").darkColor}}>
-              <Link href='/konsertquizuteliv' passHref > 
+            <Link href='/nojeuteliv' passHref >
+              <div className={style.category} style={{background: CategorySelector("Nöje & Uteliv").darkColor}}> 
                 <a><NojeUtelivIcon/></a>
-              </Link>
-            </div>
-            <div className={style.category} style={{background: CategorySelector("Sport & Fritid").darkColor}}>
-              <Link href='/sportfritid' passHref >
-                <a> <SportFritidIcon/> </a>
-              </Link>
-            </div>
-            <div className={style.category} style={{background: CategorySelector("Mat & Dryck").darkColor}}>
-              <Link href='/matdryck' passHref >
-                <a> <MatDryckIcon/> </a>
-              </Link>
-            </div>
-            <div className={style.category} style={{background: CategorySelector("Kultur & Livsstil").darkColor}}>
-              <Link href='/kulturlivsstil' passHref >
-                <a> <KulturLivsstilIcon/> </a>
-              </Link>
-            </div>
-            <div className={style.category} style={{background: CategorySelector("Hantverk & Konst").darkColor}}>
-              <Link href='/konsthantverk' passHref >
-                <a> <KonstHantverkIcon/> </a>
-              </Link>
-            </div>
+              </div>
+            </Link>
+            <Link href='/sportfritid' passHref >
+              <div className={style.category} style={{background: CategorySelector("Sport & Fritid").darkColor}}>
+                  <a> <SportFritidIcon/> </a>
+              </div>
+            </Link>
+            <Link href='/matdryck' passHref >
+              <div className={style.category} style={{background: CategorySelector("Mat & Dryck").darkColor}}>
+                  <a> <MatDryckIcon/> </a>
+              </div>
+            </Link>
+            <Link href='/kulturlivsstil' passHref >
+              <div className={style.category} style={{background: CategorySelector("Kultur & Livsstil").darkColor}}>
+                  <a> <KulturLivsstilIcon/> </a>
+              </div>
+            </Link>
+            <Link href='/hantverkkonst' passHref >
+              <div className={style.category} style={{background: CategorySelector("Hantverk & Konst").darkColor}}>
+                  <a> <KonstHantverkIcon/> </a>
+              </div>
+            </Link>
+            <Link href='/allakategorier' passHref >
             <div className={style.category} style={{background: CategorySelector("Alla kategorier").darkColor}}>
-              <Link href='/allakategorier' passHref >
                 <a> <AllaKategorier/> </a>
-              </Link>
             </div>
+            </Link>
         </div>
     </div>
   )
