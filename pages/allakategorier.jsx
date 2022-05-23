7697c21e07739fdee378ab44218baf3948949d90
Nicole <nicole.silfverling@gmail.com>
2022-05-23 17:22:36 +0200

    eventbutton on categories and desktop

--- pages/allakategorier.jsx
+++ pages/allakategorier.jsx
@@ -1,56 +1,76 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Event from '../components/Event' 
-import BackToTop from '../components/BackToTopButton'
-import Image from 'next/image'
-import React from 'react'
-import { db } from '../utils/firebase'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Event from "../components/Event";
+import BackToTop from "../components/BackToTopButton";
+import Image from "next/image";
+import React from "react";
+import { db } from "../utils/firebase";
+import Eventbutton from "../components/Eventbutton";
 
 export default function AllaKategorier({ posts }) {
-  
   return (
     <div className={styles.container}>
-      <Head><title>Unify - Alla kategorier</title></Head>
+      <Head>
+        <title>Unify - Alla kategorier</title>
+      </Head>
       <main className={styles.main}>
+        <div className={styles.eventBtn}>
+          <Eventbutton />
+        </div>
         <div className={styles.titleDiv}>
           <h1 className={styles.rubrik2}>Alla kategorier</h1>
-          <Image className={styles.cross} src="/allakategorierIcon.png" alt='allakategorierIcon' width={30} height={30}/>
-        </div> 
+          <Image
+            className={styles.cross}
+            src="/allakategorierIcon.png"
+            alt="allakategorierIcon"
+            width={30}
+            height={30}
+          />
+        </div>
         <BackToTop />
-        <Event events={posts}/> 
+        <Event events={posts} />
       </main>
-    </div> 
-     
-      )
-    }
+    </div>
+  );
+}
 
 //Server side code
-export async function getServerSideProps(){
+export async function getServerSideProps() {
   const currentDate = new Date();
-  const currentMonth = currentDate.getMonth()+1;
-  let today = '';
-  if(currentMonth<10){
-    today = currentDate.getFullYear() + '-0' + currentMonth + '-' + (currentDate.getDate());
-  }else{
-    today = currentDate.getFullYear() + '-' + currentMonth + '-' + (currentDate.getDate());
+  const currentMonth = currentDate.getMonth() + 1;
+  let today = "";
+  if (currentMonth < 10) {
+    today =
+      currentDate.getFullYear() +
+      "-0" +
+      currentMonth +
+      "-" +
+      currentDate.getDate();
+  } else {
+    today =
+      currentDate.getFullYear() +
+      "-" +
+      currentMonth +
+      "-" +
+      currentDate.getDate();
   }
 
-	const snapshot = await db
-        .collection('posts')
-        .orderBy('date')
-        .where("date", ">=", today)
-        .get()
+  const snapshot = await db
+    .collection("posts")
+    .orderBy("date")
+    .where("date", ">=", today)
+    .get();
 
   const posts = snapshot.docs.map((doc) => {
     return {
       id: doc.id,
       ...doc.data(),
-    }
-  })
+    };
+  });
 
   return {
     props: {
-      posts: JSON.parse(JSON.stringify(posts))
+      posts: JSON.parse(JSON.stringify(posts)),
     },
-  }
+  };
 }
--- pages/matdryck.jsx
+++ pages/matdryck.jsx
@@ -1,56 +1,77 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Event from '../components/Event' 
-import BackToTop from '../components/BackToTopButton'
-import Image from 'next/image'
-import React from 'react'
-import { db } from '../utils/firebase'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Event from "../components/Event";
+import BackToTop from "../components/BackToTopButton";
+import Image from "next/image";
+import React from "react";
+import { db } from "../utils/firebase";
+import Eventbutton from "../components/Eventbutton";
 
 export default function MatDryck({ events }) {
-
   return (
     <div className={styles.container}>
-      <Head><title>Unify - Mat & Dryck</title></Head>
+      <Head>
+        <title>Unify - Mat & Dryck</title>
+      </Head>
       <main className={styles.main}>
+        <div className={styles.eventBtn}>
+          <Eventbutton />
+        </div>
         <div className={styles.titleDiv}>
           <h1 className={styles.rubrik2}>Mat & Dryck</h1>
-          <Image className={styles.cross} src="/matdryckIcon.png" alt='matdryckIcon' width={30} height={30}/>
+          <Image
+            className={styles.cross}
+            src="/matdryckIcon.png"
+            alt="matdryckIcon"
+            width={30}
+            height={30}
+          />
         </div>
         <BackToTop />
-        <Event events={events} eventsKey={events.id}/> 
+        <Event events={events} eventsKey={events.id} />
       </main>
-    </div> 
-  )
+    </div>
+  );
 }
 
 //Server side code
-export async function getServerSideProps(){
+export async function getServerSideProps() {
   const currentDate = new Date();
-  const currentMonth = currentDate.getMonth()+1;
-  let today = '';
-  if(currentMonth<10){
-    today = currentDate.getFullYear() + '-0' + currentMonth + '-' + (currentDate.getDate());
-  }else{
-    today = currentDate.getFullYear() + '-' + currentMonth + '-' + (currentDate.getDate());
+  const currentMonth = currentDate.getMonth() + 1;
+  let today = "";
+  if (currentMonth < 10) {
+    today =
+      currentDate.getFullYear() +
+      "-0" +
+      currentMonth +
+      "-" +
+      currentDate.getDate();
+  } else {
+    today =
+      currentDate.getFullYear() +
+      "-" +
+      currentMonth +
+      "-" +
+      currentDate.getDate();
   }
 
-	const snapshots = await db
-    .collection('posts')
-    .orderBy('date')
+  const snapshots = await db
+    .collection("posts")
+    .orderBy("date")
     .where("date", ">=", today)
     .where("category", "==", "Mat & Dryck")
-    .get()
-  
-	const events = snapshots.docs.map((doc) => {
-		return {
-			id: doc.id,
-			...doc.data(),
-		}
-	})
+    .get();
 
-	return {
-		props: {
+  const events = snapshots.docs.map((doc) => {
+    return {
+      id: doc.id,
+      ...doc.data(),
+    };
+  });
+
+  return {
+    props: {
       events: JSON.parse(JSON.stringify(events)),
-		},
-	}
+    },
+  };
 }
